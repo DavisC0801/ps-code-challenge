6dd112eddce3c06b0e3f7e823c67a98d5bef91ff
davisc0801 <davisc1014@gmail.com>
2020-02-05 14:56:43 -0700

    Refactor into two seperate calls, due to scoping issues with the queries

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,13 +2,16 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name, :address, :post_code, :number_of_chairs
 
   def self.find_post_code_stats
-    query = select("post_code, name AS place_with_max_chairs, COUNT(id) AS total_places, SUM(number_of_chairs) AS total_chairs, (SUM(number_of_chairs)*100)/(SELECT SUM(number_of_chairs) FROM restaurants) AS chairs_pct, MAX(number_of_chairs) AS max_chairs")
+    select("post_code, COUNT(id) AS total_places, SUM(number_of_chairs) AS total_chairs, (SUM(number_of_chairs)*100.0)/(SELECT SUM(number_of_chairs) FROM restaurants) AS chairs_pct")
+    .group(:post_code)
+    .order(:post_code)
+  end
+
+  def self.find_seat_stats
+    select("name AS place_with_max_chairs, MAX(number_of_chairs) AS max_chairs")
     .where("ROW(post_code, number_of_chairs) IN (SELECT post_code, MAX(number_of_chairs) FROM restaurants GROUP BY post_code)")
     .group(:post_code, :name)
     .order(:post_code)
-
-    # RAW SQL call for reference
-    # find_by_sql("SELECT post_code, name AS place_with_max_chairs, COUNT(id) AS total_places, SUM(number_of_chairs) AS total_chairs, MAX(number_of_chairs) AS max_chairs, (SUM(number_of_chairs)*100)/(SELECT SUM(number_of_chairs) FROM restaurants) AS chairs_pct FROM restaurants WHERE ROW(post_code, number_of_chairs) IN (SELECT post_code, MAX(number_of_chairs) FROM restaurants GROUP BY post_code) GROUP BY restaurants.post_code, restaurants.name ORDER BY restaurants.post_code ASC;")
   end
 
   def determine_category
