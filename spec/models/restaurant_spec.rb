d3d981007a22b93e1e8c74e79a97e74d5cfdb8cd
Davisc0801 <davisc1014@gmail.com>
2020-02-05 16:59:43 -0700

    Refactor model tests due to sorting differences

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -27,33 +27,37 @@ RSpec.describe Restaurant, type: :model do
     it "finds the stats for restaurants grouped by post code" do
       stats = Restaurant.find_post_code_stats
       # cherry picking stats for edge cases, manually confirmed
-      expect(stats[0].post_code).to eq("LS1 2AN")
+      expect(stats[0].post_code).to eq("LS10 1JQ")
       expect(stats[0].total_places).to eq(1)
-      expect(stats[0].total_chairs).to eq(152)
-      expect(stats[0].chairs_pct).to be_within(0.01).of(7.31)
+      expect(stats[0].total_chairs).to eq(35)
+      expect(stats[0].chairs_pct).to be_within(0.01).of(1.68)
 
-      expect(stats[3].post_code).to eq("LS1 2HD")
-      expect(stats[3].total_places).to eq(3)
-      expect(stats[3].total_chairs).to eq(50)
-      expect(stats[3].chairs_pct).to be_within(0.01).of(2.41)
+      expect(stats[4].post_code).to eq("LS1 2HD")
+      expect(stats[4].total_places).to eq(3)
+      expect(stats[4].total_chairs).to eq(50)
+      expect(stats[4].chairs_pct).to be_within(0.01).of(2.41)
 
       expect(stats[-1].post_code).to eq("LS7 3PD")
       expect(stats[-1].total_places).to eq(1)
       expect(stats[-1].total_chairs).to eq(32)
       expect(stats[-1].chairs_pct).to be_within(0.01).of(1.54)
+
+      expect(stats.to_a.count).to eq(52)
     end
 
     it "finds the stats for maximum chairs groupwise to post codes" do
       stats = Restaurant.find_seat_stats
       # matching fields with above tests
-      expect(stats[0].place_with_max_chairs).to eq("Restaurant Bar and Grill")
-      expect(stats[0].max_chairs).to eq(152)
+      expect(stats[0].place_with_max_chairs).to eq("The Adelphi")
+      expect(stats[0].max_chairs).to eq(35)
 
-      expect(stats[3].place_with_max_chairs).to eq("Reds Barbecue")
-      expect(stats[3].max_chairs).to eq(40)
+      expect(stats[4].place_with_max_chairs).to eq("Reds Barbecue")
+      expect(stats[4].max_chairs).to eq(40)
 
       expect(stats[-1].place_with_max_chairs).to eq("Prohibition")
       expect(stats[-1].max_chairs).to eq(32)
+
+      expect(stats.to_a.count).to eq(52)
     end
   end
 end
