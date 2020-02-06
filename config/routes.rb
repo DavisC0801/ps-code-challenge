b0fc879db83b72435f8548baab8516edc6f7d533
Davisc0801 <davisc1014@gmail.com>
2020-02-05 18:45:39 -0700

    Open route to category view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "restaurants", to: "restaurant#index"
+  get "categories", to: "category#index"
 end
